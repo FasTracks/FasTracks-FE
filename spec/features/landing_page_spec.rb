8f436b3a286b2a022ef1cdd31fb2a3c8f2fcc71a
Sooyung Kim <kimx1279@gmail.com>
2024-01-03 20:09:59 -0700

    Add updated test and modal in view

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -2,14 +2,23 @@ require 'rails_helper'
 
 describe 'Landing Page' do
   #US 1
-  it 'has a button that requests Spotify access' do
+  it 'has a app name and a button that requests Spotify access' do
     visit root_path
 
+    expect(page).to have_content('FasTracks')
     expect(page).to have_button('Connect to Spotify to Create a Playlist')
   end
 
   it 'takes you to sign into your Spotify account when you click the button' do
+    visit root_path
     click_button('Connect to Spotify to Create a Playlist')
+
+    within('#spotifyModal', wait: 10) do
+      expect(page).to have_content('Spotify Authorization')
+      expect(page).to have_content('Connect to your Spotify account to access more features.')
+      expect(page).to have_button('Authorize on Spotify', visible: true)
+      expect(page).to have_button('Close', visible: true)
+    end
   end
 
   it 'redirects you to create a playlist page when you sign in' do
