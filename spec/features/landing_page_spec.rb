21c81f9da17c0d196f39b0d95a0da8d0b41dbdb5
Sooyung Kim <kimx1279@gmail.com>
2024-01-03 17:59:20 -0700

    Add tests and update view page

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -1,7 +1,23 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "Landing Page" do
-  it "has a button that requests Spotify access" do
-    expect().to eq
+describe 'Landing Page' do
+  #US 1
+  it 'has a button that requests Spotify access' do
+    visit root_path
+
+    expect(page).to have_button('Connect to Spotify to Create a Playlist')
+  end
+
+  it 'takes you to sign into your Spotify account when you click the button' do
+    click_button('Connect to Spotify to Create a Playlist')
+  end
+
+  it 'redirects you to create a playlist page when you sign in' do
+    
   end
 end
+
+
+# -As a visitor, when I visit the landing page, I see a button to Connect to Spotify to Create a Playlist
+# -When you click on the button, it takes you to sign into your Spotify account
+# -Once you sign in, it redirects you to create a playlist page
