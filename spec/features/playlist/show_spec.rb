b835e41c691e4433f502d5f30a94efde10ead948
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-06 14:33:30 -0700

    wrote first test for playlist_show_page

--- spec/features/playlist/show_spec.rb
+++ spec/features/playlist/show_spec.rb
@@ -8,4 +8,32 @@ RSpec.describe "Playlists#show", type: :feature do
   # -It shows a message that playlist has been successfully created
   # -It shows the playlist name, playlist length, song title and artist name
   # -It has a link to create another playlist
+
+  before(:each) do
+    genres = %w[acoustic afrobeat alt-rock alternative ambient anime black-metal bluegrass blues bossanova brazil breakbeat british cantopop chicago-house children chill classical club comedy country dance dancehall death-metal deep-house detroit-techno disco disney drum-and-bass dub dubstep edm electro electronic emo folk forro french funk garage german gospel goth grindcore groove grunge guitar happy hard-rock hardcore hardstyle heavy-metal hip-hop holidays honky-tonk house idm indian indie indie-pop industrial iranian j-dance j-idol j-pop j-rock jazz k-pop kids latin latino malay mandopop metal metal-misc metalcore minimal-techno movies mpb new-age new-release opera pagode party philippines-opm piano pop pop-film post-dubstep power-pop progressive-house psych-rock punk punk-rock r-n-b rainy-day reggae reggaeton road-trip rock rock-n-roll rockabilly romance sad salsa samba sertanejo show-tunes singer-songwriter ska sleep songwriter soul soundtracks spanish study summer swedish synth-pop tango techno trance trip-hop turkish work-out world-music]
+    allow(SpotifyApiService).to receive(:get_genres).with("fakeToken").and_return({data: {genres: genres}})
+  end
+
+  it "After selecting a genre and workout, it brings me to the playlist show page" do
+    
+    stub_request(:get, "http://localhost:3000/api/v1/playlists").
+         with(
+           body: {"{\"token\":\"fakeToken\",\"genre\":\"Pop\",\"workout\":\"Cardio\",\"playlist_name\":\"FasTracks Pop Cardio\"}"=>nil},
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type'=>'application/x-www-form-urlencoded',
+          'User-Agent'=>'Faraday v2.8.1'
+           }).
+         to_return(status: 200, body: "", headers: {})
+    
+    visit "/generate_playlist?tkn=fakeToken"
+    
+    select "Pop", from: :genre
+    select "Cardio", from: :workout
+    
+    click_button("Create My Playlist")
+
+    expect(current_path).to eq("/playlist")
+  end
 end
