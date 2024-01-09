28fe49615b51e679e8b714650ed8bf8e84d50868
Sooyung Kim <kimx1279@gmail.com>
2024-01-09 11:47:18 -0700

    Update fixture data, update test environment caching memorization

--- app/controllers/welcome_controller.rb
+++
@@ -1,7 +0,0 @@
-require "./config/spotify_credentials"
-
-class WelcomeController < ApplicationController
-  def index
-    @CLIENT_ID = CLIENT_ID
-  end
-end
--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-require "./config/spotify_credentials"
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, CLIENT_ID, CLIENT_SECRET, scope: %w(
-    playlist-read-private
-    user-read-private
-    user-read-email
-  ).join(' ')
-end
