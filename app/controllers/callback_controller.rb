9ae1463b5e5b071b1a18cc5b0929a7f0e3bd5274
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-10 16:09:44 -0700

    made changes

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -3,6 +3,7 @@ class CallbackController < ApplicationController
     auth_code = params[:code] # the auth code is passed in the URL (via link in landing pag) in after the user has been redirected back to the app
     # auth code needs to be exchanged for an access token
     access_token = SpotifyApiService.request_token(auth_code)[:data][:access_token]
+    # require 'pry'; binding.pry
     redirect_to "/generate_playlist?tkn=#{access_token}"
   end
 end
