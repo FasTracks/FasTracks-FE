24ae30d15f1582e81bfbd00a3dba4f0623d80fb3
Scott DeVoss <scottdevoss@yahoo.com>
2024-01-10 14:58:44 -0700

    fixed test

--- app/services/spotify_api_service.rb
+++ app/services/spotify_api_service.rb
@@ -70,4 +70,12 @@ class SpotifyApiService
   def self.response_conversion(response)
     {status: response.status, data: JSON.parse(response.body, symbolize_names: true)}
   end
+
+  def self.callback_url
+    if Rails.env.test? 
+      Rails.application.routes.url_helpers.callback_url(host: "localhost:5000")
+    else 
+      Rails.application.routes.url_helpers.callback_url
+    end
+  end
 end
